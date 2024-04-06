b7ceff74264345283490c89bc3f51ac568ec9bc9
GergelyRoka <gergelyroka90@gmail.com>
2024-04-06 18:30:01 +0200

    part one of matrices done

--- include/matrices.h
+++ include/matrices.h
@@ -6,7 +6,7 @@
 /*   By: groka <groka@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 19:15:08 by groka             #+#    #+#             */
-/*   Updated: 2024/04/02 21:48:51 by groka            ###   ########.fr       */
+/*   Updated: 2024/04/06 17:02:05 by groka            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,21 @@ typedef struct s_matrix
 	double	matrix[4][4];
 }	t_matrix;
 
+//matrices_a.c
 bool		is_equal_matrix(t_matrix a, t_matrix b);
 t_matrix	multiply_matrix(t_matrix a, t_matrix b);
 t_tuple		multiply_matrix_tuple(t_matrix a, t_tuple b);
 t_matrix	identity_matrix(void);
 t_matrix	transpose_matrix(t_matrix a);
-void		print_matrix(t_matrix a);
+
+// matrices_b.c
 double		minor_matrix(t_matrix a, int x, int y);
+double		cofactor_matrix(t_matrix a, int x, int y);
+double		determinant_matrix(t_matrix a);
+t_matrix	submatrix(t_matrix a, int row, int col);
+
+// matrices_c.c
+bool		is_invertible_matrix(t_matrix a);
+t_matrix	inverse_matrix(t_matrix a);
 
 #endif
