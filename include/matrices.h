b5218bae58a92e275f7f75af5b48e03a546e1960
GergelyRoka <gergelyroka90@gmail.com>
2024-04-07 17:30:23 +0200

    update

--- include/matrices.h
+++ include/matrices.h
@@ -6,7 +6,7 @@
 /*   By: groka <groka@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 19:15:08 by groka             #+#    #+#             */
-/*   Updated: 2024/04/06 20:40:57 by groka            ###   ########.fr       */
+/*   Updated: 2024/04/07 17:09:42 by groka            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,12 +41,14 @@ t_matrix	submatrix(t_matrix a, int row, int col);
 bool		is_invertible_matrix(t_matrix a);
 t_matrix	inverse_matrix(t_matrix a);
 
-// tranformations_a.c
+// transformations_a.c
 t_matrix	translation_matrix(double x, double y, double z);
 t_matrix	scaling_matrix(double x, double y, double z);
 t_matrix	rotation_x_matrix(double angle);
 t_matrix	rotation_y_matrix(double angle);
 t_matrix	rotation_z_matrix(double angle);
 
+// transformations_b.c
+t_matrix	shearing_matrix(t_tuple x, t_tuple y, t_tuple z);
 
 #endif
