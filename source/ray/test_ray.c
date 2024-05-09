f235a8ff22c9d9d8e9008def06099f9d94947d27
Gergely Roka <groka@student.42berlin.de>
2024-05-09 16:37:27 +0200

    test_ray

--- source/ray/test_ray.c
+++ source/ray/test_ray.c
@@ -6,10 +6,11 @@
 /*   By: groka <groka@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 21:22:19 by groka             #+#    #+#             */
-/*   Updated: 2024/04/07 21:23:15 by groka            ###   ########.fr       */
+/*   Updated: 2024/05/09 15:34:23 by groka            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../include/minirt.h"
 #include "../../include/ray.h"
 #include <stdio.h>
 
@@ -38,8 +39,73 @@ int	main(void)
 	ray.origin = do_point(2, 3, 4);
 	ray.direction = do_vector(1, 0, 0);
 
-	print_tuple(position(ray, 0.0));
-	print_tuple(position(ray, 1.0));
-	print_tuple(position(ray, -1.0));
-	print_tuple(position(ray, 2.5));
+	printf("test\n");
+	t_ray	ray2;
+	ray2 = do_ray(do_point(2, 3, 4), do_vector(1, 0, 0));
+
+	print_tuple(position(ray2, 0.0));
+	print_tuple(position(ray2, 1.0));
+	print_tuple(position(ray2, -1.0));
+	print_tuple(position(ray2, 2.5));
+
+	{
+		t_sphere	sphere;
+		t_ray		ray;
+		t_intersection	xs;
+
+		sphere.coordinate = do_point(0, 0, 0);
+		sphere.diameter = 1.0;
+		ray.origin = do_point(0, 0, -5);
+		ray.direction = do_vector(0, 0, 1);
+		xs = intersect_sphere(sphere, ray);
+		printf("%d %f %f\n", xs.count, xs.xs[0], xs.xs[1]);
+	}
+	{
+		t_sphere	sphere;
+		t_ray		ray;
+		t_intersection	xs;
+
+		sphere.coordinate = do_point(0, 0, 0);
+		sphere.diameter = 1.0;
+		ray.origin = do_point(0, 1, -5);
+		ray.direction = do_vector(0, 0, 1);
+		xs = intersect_sphere(sphere, ray);
+		printf("%d %f %f\n", xs.count, xs.xs[0], xs.xs[1]);
+	}
+	{
+		t_sphere	sphere;
+		t_ray		ray;
+		t_intersection	xs;
+
+		sphere.coordinate = do_point(0, 0, 0);
+		sphere.diameter = 1.0;
+		ray.origin = do_point(0, 2, -5);
+		ray.direction = do_vector(0, 0, 1);
+		xs = intersect_sphere(sphere, ray);
+		printf("%d %f %f\n", xs.count, xs.xs[0], xs.xs[1]);
+	}
+	{
+		t_sphere	sphere;
+		t_ray		ray;
+		t_intersection	xs;
+
+		sphere.coordinate = do_point(0, 0, 0);
+		sphere.diameter = 1.0;
+		ray.origin = do_point(0, 0, 0);
+		ray.direction = do_vector(0, 0, 1);
+		xs = intersect_sphere(sphere, ray);
+		printf("%d %f %f\n", xs.count, xs.xs[0], xs.xs[1]);
+	}
+	{
+		t_sphere	sphere;
+		t_ray		ray;
+		t_intersection	xs;
+
+		sphere.coordinate = do_point(0, 0, 0);
+		sphere.diameter = 1.0;
+		ray.origin = do_point(0, 0, 5);
+		ray.direction = do_vector(0, 0, 1);
+		xs = intersect_sphere(sphere, ray);
+		printf("%d %f %f\n", xs.count, xs.xs[0], xs.xs[1]);
+	}
 }
